9adecf4c7e46b0de94372132e5f800f821932a5c
nao-uru <nao.uru0419@gmail.com>
2023-01-31 12:44:57 +0900

    Add Card

--- pages/Contact.tsx
+++ pages/Contact.tsx
@@ -1,6 +1,6 @@
 import Footer from '@/components/Footer '
 import HeaderNav from '@/components/HeaderNav'
-import { ChakraProvider, Container, Heading } from '@chakra-ui/react'
+import { Box, ChakraProvider, Container, Heading, Text } from '@chakra-ui/react'
 
 export default function ContactPage() {
 
@@ -8,8 +8,12 @@ export default function ContactPage() {
         <>
         <ChakraProvider>
         <HeaderNav />
-        <Container h={'500px'}>
-        <Heading fontSize={'md'}>This is Contact</Heading>
+        <Container mt={'8'}>
+        <Heading fontSize={'2xl'}>お問い合わせ</Heading>
+        <Text>お問い合わせはこちらのフォームから受け付けております。お気軽にお問い合わせください！</Text>
+        <Box mt={'4'} mb={'16'} w={'full'}>
+        <iframe src="https://docs.google.com/forms/d/e/1FAIpQLSfXtSSUTsuCr5040D0_Ynyf7APUEDU42FMzmLC4famcuLlBuw/viewform?embedded=true" width="100%" height="1000px" scrolling="no">読み込んでいます…</iframe>
+        </Box>
         </Container>
         <Footer />
         </ChakraProvider>
